fab0159e84a085d46dd0cb7c36bcfbafa44f8979
WarEagle451 <56392435+WarEagle451@users.noreply.github.com>
2024-02-11 17:11:09 -0500

    Additions
    algorithm.hpp
    string.hpp

---
+++ include/myl/algorithm.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <myl/definitions.hpp>
+
+#include <concepts>
+
+#ifdef min // War on Microsoft's min macro
+#  undef min
+#endif
+
+#ifdef max // War on Microsoft's max macro
+#  undef max
+#endif
+
+namespace myl {
+    template<typename T>
+	MYL_NO_DISCARD constexpr auto min(const T a, const T b) -> T {
+		return a < b ? a : b;
+	}
+
+	template<typename T, std::same_as<T>... Args>
+	MYL_NO_DISCARD constexpr auto min(const T a, const T b, const Args... args) -> T {
+		return min(min(a, b), args...);
+	}
+
+	template<typename T>
+	MYL_NO_DISCARD constexpr auto max(const T a, const T b) -> T {
+		return a > b ? a : b;
+	}
+
+	template<typename T, std::same_as<T>... Args>
+	MYL_NO_DISCARD constexpr auto max(const T a, const T b, const Args... args) -> T {
+		return max(max(a, b), args...);
+	}
+
+	template<typename T>
+	MYL_NO_DISCARD constexpr auto clamp(const T v, const T min, const T max) -> T {
+		return v < min ? min : v > max ? max : v;
+	}
+
+	template<typename T>
+	MYL_NO_DISCARD constexpr auto floor(const T v, const T floor) -> T {
+		return v > floor ? v : floor;
+	}
+
+	template<typename T>
+	MYL_NO_DISCARD constexpr auto ceil(const T v, const T ceil) -> T {
+		return v > ceil ? ceil : v;
+	}
+
+	template<typename T>
+	MYL_NO_DISCARD constexpr auto difference(const T a, const T b) -> T {
+		return a > b ? a - b : b - a;
+	}
+}
